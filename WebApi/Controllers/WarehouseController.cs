5b0208639c4b17212d86877be0e68e394e0870ee
mhracki <mhracki@gmail.com>
2019-11-14 21:20:09 +0100

    WarehouseController Crud update

--- WebApi/Controllers/WarehouseController.cs
+++ WebApi/Controllers/WarehouseController.cs
@@ -39,8 +39,8 @@ namespace WebApi.Controllers
 
 
         [HttpGet]
-        [Route("List")]
-        //GET : /api/Warehouse/Item
+        [Route("item/get")]
+        //GET : /api/Warehouse/item/get
         public ActionResult<IEnumerable<string>> Get()
         {
             //string connectionString = configuration.GetConnectionString("IdentityConnection");
@@ -103,8 +103,8 @@ namespace WebApi.Controllers
         }
 
         [HttpPut("{id}")]
-        //PUT : /api/Warehouse/putitem/id
-        [Route("PutItem/{id}")]
+        //PUT : /api/Warehouse/item/put/id
+        [Route("item/put/{id}")]
         public async Task<IActionResult> PutItem(Guid id,[FromBody]ItemModel model)
         {
 
@@ -149,8 +149,8 @@ namespace WebApi.Controllers
         }
 
         [HttpDelete("{id}")]
-        //DELETE : /api/Warehouse/item/id
-        [Route("DelItem/{id}")]
+        //DELETE : /api/Warehouse/item/delete/id
+        [Route("item/delete/{id}")]
         public async Task<IActionResult> Delete(Guid id)
         {
 
@@ -173,7 +173,7 @@ namespace WebApi.Controllers
         }
 
         [HttpPost]
-        [Route("Item")]
+        [Route("item/post")]
         //POST : /api/Warehouse/newItem
         public async Task<Object> PostWarehauseItem (ItemModel model)
         {
@@ -206,7 +206,7 @@ namespace WebApi.Controllers
             }
         }
         [HttpGet]
-        [Route("Warehouse/get")]
+        [Route("warehouse/get")]
         //GET : /api/Warehouse/warehouse/get
         public ActionResult<IEnumerable<string>> GetWarehouse()
         {
@@ -224,7 +224,7 @@ namespace WebApi.Controllers
         }
 
         [HttpPost]
-        [Route("Warehouse/post")]
+        [Route("warehouse/post")]
         //POST : /api/Warehouse/Warehouse/post
         public async Task<Object> PostWarehause(WarehouseModel model)
         {
@@ -297,7 +297,7 @@ namespace WebApi.Controllers
         }
 
         [HttpGet("{warehouseId}")]
-        [Route("Room/get/{warehouseId}")]
+        [Route("room/get/{warehouseId}")]
         //GET : /api/Warehouse/room/get/{id}
         public ActionResult<IEnumerable<string>> GetRoom(Guid warehouseId)
         {
@@ -315,8 +315,84 @@ namespace WebApi.Controllers
 
         }
 
+        [HttpPost]
+        [Route("room/post")]
+        //POST : /api/Warehouse/room/post
+        public async Task<Object> PostRoom(RoomModel model)
+        {
+
+            var room = new RoomModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                WarehouseId = model.WarehouseId
+
+            };
+
+            try
+            {
+                var result = await _context.Room.AddAsync(room);
+                await _context.SaveChangesAsync();
+                return Ok(room);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpPut("{id}")]
+        //PUT : /api/Warehouse/room/put/id
+        [Route("room/put/{id}")]
+        public async Task<IActionResult> PutRoom(Guid id, [FromBody]RoomModel model)
+        {
+
+            var room = new RoomModel()
+            {
+                Id = id,
+                Name = model.Name,
+                WarehouseId = model.WarehouseId,
+
+            };
+
+            try
+            {
+                _context.Entry(room).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(room);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete("{id}")]
+        //DELETE : /api/Warehouse/room/delete/id
+        [Route("room/delete/{id}")]
+        public async Task<IActionResult> DeleteRoom(Guid id)
+        {
+
+            var room = await _context.Room.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Room.Remove(room);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
         [HttpGet("{roomId}")]
-        [Route("Column/get/{roomId}")]
+        [Route("column/get/{roomId}")]
         //GET : /api/Warehouse/column/get/{id}
         public ActionResult<IEnumerable<string>> GetColumn(Guid roomId)
         {
@@ -333,6 +409,82 @@ namespace WebApi.Controllers
             return Ok(result);
 
         }
+        [HttpPost]
+        [Route("column/post")]
+        //POST : /api/Warehouse/column/post
+        public async Task<Object> PostColumn(ColumnModel model)
+        {
+
+            var column = new ColumnModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                RoomId = model.RoomId,
+
+            };
+
+            try
+            {
+                var result = await _context.Columns.AddAsync(column);
+                await _context.SaveChangesAsync();
+                return Ok(column);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpPut("{id}")]
+        //PUT : /api/Warehouse/column/put/id
+        [Route("column/put/{id}")]
+        public async Task<IActionResult> PutColumn(Guid id, [FromBody]ColumnModel model)
+        {
+
+            var column = new ColumnModel()
+            {
+                Id = id,
+                Name = model.Name,
+                RoomId = model.RoomId,
+
+            };
+
+            try
+            {
+                _context.Entry(column).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(column);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete("{id}")]
+        //DELETE : /api/Warehouse/column/delete/id
+        [Route("column/delete/{id}")]
+        public async Task<IActionResult> DeleteColumn(Guid id)
+        {
+
+            var column = await _context.Columns.FindAsync(id);
+            if (column == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Columns.Remove(column);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
         [HttpGet("{columnId}")]
         [Route("Rack/get/{columnId}")]
         //GET : /api/Warehouse/rack/get/{id}
@@ -351,6 +503,83 @@ namespace WebApi.Controllers
             return Ok(result);
 
         }
+        [HttpPost]
+        [Route("rack/post")]
+        //POST : /api/Warehouse/rack/post
+        public async Task<Object> PostRack(RackModel model)
+        {
+
+            var rack = new RackModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                ColumnsId = model.ColumnsId,
+
+            };
+
+            try
+            {
+                var result = await _context.Rack.AddAsync(rack);
+                await _context.SaveChangesAsync();
+                return Ok(rack);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpPut("{id}")]
+        //PUT : /api/Warehouse/rack/put/id
+        [Route("rack/put/{id}")]
+        public async Task<IActionResult> PutRack(Guid id, [FromBody]RackModel model)
+        {
+
+            var rack = new RackModel()
+            {
+                Id = id,
+                Name = model.Name,
+                ColumnsId = model.ColumnsId
+
+            };
+
+            try
+            {
+                _context.Entry(rack).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(rack);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete("{id}")]
+        //DELETE : /api/Warehouse/rack/delete/id
+        [Route("rack/delete/{id}")]
+        public async Task<IActionResult> DeleteRack(Guid id)
+        {
+
+            var rack = await _context.Rack.FindAsync(id);
+            if (rack == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Rack.Remove(rack);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
         [HttpGet("{rackId}")]
         [Route("Side/get/{rackId}")]
         //GET : /api/Warehouse/side/get/{id}
@@ -369,6 +598,81 @@ namespace WebApi.Controllers
             return Ok(result);
 
         }
+        [HttpPost]
+        [Route("side/post")]
+        //POST : /api/Warehouse/side/post
+        public async Task<Object> PostSide(SideModel model)
+        {
+
+            var side = new SideModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                RackId = model.RackId
+
+            };
+
+            try
+            {
+                var result = await _context.Side.AddAsync(side);
+                await _context.SaveChangesAsync();
+                return Ok(side);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpPut("{id}")]
+        //PUT : /api/Warehouse/side/put/id
+        [Route("side/put/{id}")]
+        public async Task<IActionResult> PutSide(Guid id, [FromBody]SideModel model)
+        {
+
+            var side = new SideModel()
+            {
+                Id = id,
+                Name = model.Name,
+                RackId = model.RackId,
+
+            };
+
+            try
+            {
+                _context.Entry(side).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(side);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete("{id}")]
+        //DELETE : /api/Warehouse/side/delete/id
+        [Route("side/delete/{id}")]
+        public async Task<IActionResult> DeleteSide(Guid id)
+        {
+
+            var side = await _context.Side.FindAsync(id);
+            if (side == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Side.Remove(side);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
         [HttpGet("{sackId}")]
         [Route("Shelf/get/{sideId}")]
         //GET : /api/Warehouse/shelf/get/{id}
@@ -387,6 +691,80 @@ namespace WebApi.Controllers
             return Ok(result);
 
         }
+        [HttpPost]
+        [Route("shelf/post")]
+        //POST : /api/Warehouse/shelf/post
+        public async Task<Object> PostShelf(ShelfModel model)
+        {
+
+            var shelf = new ShelfModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                SideId = model.SideId,
+
+            };
+
+            try
+            {
+                var result = await _context.Shelf.AddAsync(shelf);
+                await _context.SaveChangesAsync();
+                return Ok(shelf);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpPut("{id}")]
+        //PUT : /api/Warehouse/shelf/put/id
+        [Route("shelf/put/{id}")]
+        public async Task<IActionResult> PutShelf(Guid id, [FromBody]ShelfModel model)
+        {
+
+            var shelf = new ShelfModel()
+            {
+                Id = id,
+                Name = model.Name,
+                SideId = model.SideId,
+
+            };
+
+            try
+            {
+                _context.Entry(shelf).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(shelf);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete("{id}")]
+        //DELETE : /api/Warehouse/shelf/delete/id
+        [Route("shelf/delete/{id}")]
+        public async Task<IActionResult> DeleteShelf(Guid id)
+        {
+
+            var shelf = await _context.Shelf.FindAsync(id);
+            if (shelf == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Shelf.Remove(shelf);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         [HttpGet("{shelfId}")]
         [Route("Place/get/{shelfId}")]
         //GET : /api/Warehouse/place/get/{id}
@@ -404,6 +782,80 @@ namespace WebApi.Controllers
 
             return Ok(result);
 
+        }
+        [HttpPost]
+        [Route("place/post")]
+        //POST : /api/Warehouse/place/post
+        public async Task<Object> PostPlace(PlaceModel model)
+        {
+
+            var place = new PlaceModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                ShelfId=model.ShelfId
+
+            };
+
+            try
+            {
+                var result = await _context.Place.AddAsync(place);
+                await _context.SaveChangesAsync();
+                return Ok(place);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpPut("{id}")]
+        //PUT : /api/Warehouse/place/put/id
+        [Route("place/put/{id}")]
+        public async Task<IActionResult> PutPlace(Guid id, [FromBody]PlaceModel model)
+        {
+
+            var place = new PlaceModel()
+            {
+                Id = id,
+                Name = model.Name,
+                ShelfId = model.ShelfId
+
+            };
+
+            try
+            {
+                _context.Entry(place).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(place);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete("{id}")]
+        //DELETE : /api/Warehouse/place/delete/id
+        [Route("place/delete/{id}")]
+        public async Task<IActionResult> DeletePlace(Guid id)
+        {
+
+            var place = await _context.Place.FindAsync(id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Place.Remove(place);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
